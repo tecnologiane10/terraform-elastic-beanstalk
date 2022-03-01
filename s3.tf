bec4869d48e1b97003a03e4da0281d735562aa78
Olivério Neto <oliveriojnn@gmail.com>
2022-03-01 08:47:59 -0300

    Add server_side_encryption_configuration in s3

--- s3.tf
+++ s3.tf
@@ -2,6 +2,14 @@ resource "aws_s3_bucket" "beanstalk_deploy" {
   bucket = "${var.environment_name}-deploy"
   acl    = "private"
 
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
   tags {
     Name = "${var.environment_name}-deploy"
   }
